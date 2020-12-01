9505a53b15000fad0da0ff5ccca86561ae79a992
MannerheimLine <Valehtelit@mail.ru>
2020-12-01 20:08:40 +1000

    .htaccess added

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
 #Автозагрузка
+use Sandbox\Application\ExampleClass;
+
 chdir(dirname(__DIR__));
 require "vendor/autoload.php";
 
 $a = 1;
 $b = 2;
-$obj = new \Sandbox\Application\ExampleClass();
+$obj = new ExampleClass();
 $obj->hello($a, $b);
 echo "Thats work fine";
